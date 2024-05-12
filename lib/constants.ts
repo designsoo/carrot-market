d348114e2ee754f0931ef867981b6adaff4b4fd8
designsoo <kheesoo10@gmail.com>
2024-05-12 20:28:07 +0900

    faet: 패스워드 유효성 검사 관련 재사용 가능한 변수들 상수 파일로 분리

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const PASSWORD_MIN_LENGTH = 4;
+
+export const PASSWORD_REGEX = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/,
+);
+
+export const PASSWORD_REGEX_ERROR =
+  'At least one uppercase letter, one lowercase letter, one number and one special character';
