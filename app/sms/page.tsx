7b3d4a0bcde334559f2f57ebf6e203464be6e383
designsoo <kheesoo10@gmail.com>
2024-05-12 20:26:39 +0900

    feat: SMS 페이지-useFormState 훅 적용 및 input name 및 key 추가, error props 적용

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,17 +1,46 @@
-import FormInput from '@/components/form-input';
-import FormButton from '@/components/form-btn';
+'use client';
+
+import Input from '@/components/input';
+import Button from '@/components/button';
+import { useFormState } from 'react-dom';
+import { smsLogIn } from './actions';
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
 export default function SMSLogIn() {
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
+
   return (
     <div className='flex flex-col gap-10 px-6 py-8'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>SNS Login</h1>
         <h2 className='text-xl'>Verify your phone number.</h2>
       </div>
-      <form className='flex flex-col gap-3'>
-        <FormInput type='number' placeholder='Phone number' required errors={[]} />
-        <FormInput type='number' placeholder='Verification code' required errors={[]} />
-        <FormButton loading={false} text='Verfiy' />
+      <form action={dispatch} className='flex flex-col gap-3'>
+        {state.token ? (
+          <Input
+            key='token'
+            name='token'
+            type='number'
+            placeholder='Verification code'
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            key='phone'
+            name='phone'
+            type='text'
+            placeholder='Phone number'
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? 'Verify Token' : 'Send Varification SMS '} />
       </form>
     </div>
   );
