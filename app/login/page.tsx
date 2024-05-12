81c19ae9f5fa86fad2f2b360afc9529fd184e353
designsoo <kheesoo10@gmail.com>
2024-05-12 20:24:43 +0900

    feat: 로그인-useFormState 훅 적용 및 input name 속성 추가, error props 적용

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,37 @@
-import FormInput from '@/components/form-input';
-import FormButton from '@/components/form-btn';
+'use client';
+
+import Input from '@/components/input';
+import Button from '@/components/button';
 import SocialLogin from '@/components/social-login';
+import { useFormState } from 'react-dom';
+import { Login } from './actions';
+import { PASSWORD_MIN_LENGTH } from '@/lib/constants';
 
 export default function LogIn() {
+  const [state, dispatch] = useFormState(Login, null);
   return (
     <div className='flex flex-col gap-10 px-6 py-8'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>안녕하세요!</h1>
         <h2 className='text-xl'>Log in wirh email and password.</h2>
       </div>
-      <form className='flex flex-col gap-3'>
-        <FormInput type='email' placeholder='Email' required errors={[]} />
-        <FormInput type='Password' placeholder='Password' required errors={[]} />
-        <FormButton loading={false} text='Create account' />
+      <form action={dispatch} className='flex flex-col gap-3'>
+        <Input
+          name='email'
+          type='email'
+          placeholder='Email'
+          required
+          errors={state?.fieldErrors.email}
+        />
+        <Input
+          name='password'
+          type='Password'
+          placeholder='Password'
+          required
+          errors={state?.fieldErrors.password}
+          minLength={PASSWORD_MIN_LENGTH}
+        />
+        <Button text='Log in' />
       </form>
       <SocialLogin />
     </div>
