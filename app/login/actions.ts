48c0bd96d848d99c635277935c884a63f1f99ecb
designsoo <kheesoo10@gmail.com>
2024-05-13 23:22:11 +0900

    feat: 로그인 시 입력하는 email 입력값과 DB 데이터 중복 확인, 비밀번호 일치 여부 확인, 세션 저장, 로그인 에러 처리

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,16 +5,36 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import getsession from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  password: z
-    .string({
-      required_error: 'Password is required',
-    })
-    .min(PASSWORD_MIN_LENGTH)
-    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, 'An account with this email does not exist.'),
+  password: z.string({
+    required_error: 'Password is required',
+  }),
+  // .min(PASSWORD_MIN_LENGTH)
+  // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
 export const Login = async (prevState: any, formData: FormData) => {
@@ -23,11 +43,37 @@ export const Login = async (prevState: any, formData: FormData) => {
     password: formData.get('password'),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    return console.log(result.data);
+    // 등록된 유저인지 확인
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    // 사용자 입력 값과 저장된 패스워드 일치 여부 확인(입력값, 데이터 베이스 해시 값)
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? '');
+
+    // 쿠키 저장
+    if (ok) {
+      const session = await getsession();
+      session.id = user!.id;
+      await session.save();
+      redirect('/profile');
+    } else {
+      return {
+        fieldErrors: {
+          password: ['Wrong password.'],
+          email: [],
+        },
+      };
+    }
   }
 };
