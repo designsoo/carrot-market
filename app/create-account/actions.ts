ab47c2e41143cabee3b16aea3baa649c3806e500
designsoo <kheesoo10@gmail.com>
2024-05-13 23:21:25 +0900

    feat: 회원가입 시 입력하는 username, email 입력값과 DB 데이터 중복 확인, 비밀번호 해싱, 세션 저장

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,8 +4,11 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
-
+import bcrypt from 'bcrypt';
+import { redirect } from 'next/navigation';
+import getSesstion from '@/lib/session';
 interface checkPasswordsProps {
   password: string;
   confirm_password: string;
@@ -15,6 +18,32 @@ const checkUsername = (username: string) => !username.includes('potato');
 
 const checkPasswords = ({ password, confirm_password }: checkPasswordsProps) =>
   password === confirm_password;
+
+// DB에 있는 데이터와 중복되는지 확인(username)
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+// DB에 있는 데이터와 중복되는지 확인(email)
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user) === false;
+};
 
 const fromSchema = z
   .object({
@@ -27,12 +56,15 @@ const fromSchema = z
       .max(10, 'That is too loooog!!')
       .toLowerCase()
       .trim()
-      .refine(checkUsername, '포테이토를 제외하고 사용하세요'),
-    email: z.string().email().toLowerCase(),
-    password: z
+      .refine(checkUsername, 'No potatoes allowed!!!')
+      .refine(checkUniqueUsername, 'This username is already taken'),
+    email: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, 'There is an account already registered with that email'),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPasswords, {
@@ -48,10 +80,27 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get('confirm_password'),
   };
 
-  const result = fromSchema.safeParse(data);
+  const result = await fromSchema.safeParseAsync(data);
+
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log('login');
+    // hash password
+    const hashedPassword = await bcrypt.hash(result.data.password, 12); //해싱 알고리즘 12번 실행
+    // save the user to DB
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const session = await getSesstion();
+    session.id = user.id;
+    await session.save();
+    redirect('/profile');
   }
 }
