5fe3adac8dfaa1d1373c4d18f5b46df6945689f7
designsoo <kheesoo10@gmail.com>
2024-05-13 23:02:46 +0900

    refactor: 메인페이지 헤드라인 오타 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LogIn() {
     <div className='flex flex-col gap-10 px-6 py-8'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>안녕하세요!</h1>
-        <h2 className='text-xl'>Log in wirh email and password.</h2>
+        <h2 className='text-xl'>Log in with email and password.</h2>
       </div>
       <form action={dispatch} className='flex flex-col gap-3'>
         <Input
