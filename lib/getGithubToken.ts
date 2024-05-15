6a0f0a98fafde7356c7e8569f29fe1cea9a22cbf
designsoo <kheesoo10@gmail.com>
2024-05-15 16:31:14 +0900

    feat: code를 POST하고 받아온 access token을 반환하는 함수 구현

---
+++ lib/getGithubToken.ts
@@ -0,0 +1,22 @@
+const getGithubToken = async (code: string) => {
+  const accessTokenParams = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID!,
+    client_secret: process.env.GITHUB_CLIENT_SECRET!,
+    code,
+  }).toString();
+
+  const accessTokenURL = `https://github.com/login/oauth/access_token?${accessTokenParams}`;
+
+  const { error, access_token } = await (
+    await fetch(accessTokenURL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+  ).json();
+
+  return { error, access_token };
+};
+
+export default getGithubToken;
