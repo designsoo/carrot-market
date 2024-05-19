1b98f3d99cfc29d9764ef20604647dc36f0be791
designsoo <kheesoo10@gmail.com>
2024-05-19 19:14:03 +0900

    feat: 상품 리스트용 카트 컴포넌트 구현

---
+++ components/list-product.tsx
@@ -0,0 +1,35 @@
+import { formatToTimeAgo, formatToWon } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface ListProductProps {
+  id: number;
+  title: string;
+  price: number;
+  photo: string;
+  created_at: Date;
+}
+
+export default function ListProduct({
+  id,
+  title,
+  price,
+  photo,
+  created_at,
+}: ListProductProps) {
+  return (
+    <Link href={`/products/${id}`} className='flex gap-5'>
+      <div className='relative size-28 shrink-0 overflow-hidden rounded-md'>
+        <Image fill src={photo} alt={title} />
+      </div>
+      <div className='flex flex-col gap-1 *:text-white'>
+        <span className='text-lg'>{title}</span>
+        <span className='text-sm text-neutral-500'>
+          {formatToTimeAgo(created_at.toString())}
+        </span>
+        <span className='text-lg font-semibold'>{formatToWon(price)}원</span>
+      </div>
+    </Link>
+  );
+}
