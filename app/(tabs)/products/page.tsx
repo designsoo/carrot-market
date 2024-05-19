3b5dd3984a9983b82e52131e406ae1f20ac8dd66
designsoo <kheesoo10@gmail.com>
2024-05-20 01:36:59 +0900

    refactor: 상품 리스트 페이지 구현

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,8 @@
-import ListProduct from '@/components/list-product';
+import ProductList from '@/components/product-list';
 import db from '@/lib/db';
+import { Prisma } from '@prisma/client';
 
-async function getProducts() {
+async function getInitialProducts() {
   const products = await db.product.findMany({
     select: {
       id: true,
@@ -10,19 +11,23 @@ async function getProducts() {
       photo: true,
       created_at: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: 'desc',
+    },
   });
 
   return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<typeof getInitialProducts>;
+
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getInitialProducts();
 
   return (
     <div className='flex flex-col gap-5 p-5'>
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
