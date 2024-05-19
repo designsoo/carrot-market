4245d5ae2c4b203998ecb1a501e0ce76c710bf23
designsoo <kheesoo10@gmail.com>
2024-05-19 19:16:06 +0900

    feat: 상품 리스트 페이지 구현

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,28 @@
-export default function Products() {
+import ListProduct from '@/components/list-product';
+import db from '@/lib/db';
+
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      photo: true,
+      created_at: true,
+    },
+  });
+
+  return products;
+}
+
+export default async function Products() {
+  const products = await getProducts();
+
   return (
-    <div className='text-4xl text-white'>
-      <h1>Products!</h1>
+    <div className='flex flex-col gap-5 p-5'>
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
