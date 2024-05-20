d5a0cf96a14bc875b11cf5e6679546cc7a084e59
designsoo <kheesoo10@gmail.com>
2024-05-20 21:36:13 +0900

    feat: Next.config 파일에 cloudflare hostname 추가

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/u/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
