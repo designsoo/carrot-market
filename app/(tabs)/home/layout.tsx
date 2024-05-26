626b6f3d78cc2e499a67182caf3bf475f91ce6ed
designsoo <kheesoo10@gmail.com>
2024-05-27 06:15:15 +0900

    feat: 제품 상세보기 모달과 /home의 productList를 함께 보여주기 위한 layout 구현

---
+++ app/(tabs)/home/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface HomeLayoutProps {
+  children: ReactNode;
+  modal: ReactNode;
+}
+
+export default function HomeLayout({ children, modal }: HomeLayoutProps) {
+  return (
+    <>
+      {children}
+      {modal}
+    </>
+  );
+}
