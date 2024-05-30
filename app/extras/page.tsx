b18ad3df711346417661f296ba44c9d4315446e5
designsoo <kheesoo10@gmail.com>
2024-05-30 21:17:11 +0900

    feat: 추가한 jakarta, rubik 폰트 Extras 페이지 적용

---
+++ app/extras/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Extras() {
+  return (
+    <div className='flex flex-col gap-3 py-10'>
+      <h1 className='font-rubik text-6xl'>Extras!</h1>
+      <h2 className='font-jakarta'>So much more to learn!</h2>
+    </div>
+  );
+}
